a398bfd1fcc8d916230b081cabc3f647d145ae73
code4me2me <32439114+code4me2me@users.noreply.github.com>
2019-02-24 21:20:02 -0500

    Mate DE & default directory changes
    Remove directory set option

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
-while getopts d: option
-do
- case "${option}"
- in
- d) DIR=$OPTARG;;
- esac
-done
-
-
-URL=$(wget -O - http://www.reddit.com/r/wallpapers.rss | grep -Eo "https://?[^&]+jpg" | grep -v "thumbs" | head -1);
+URL=$(wget -qO - http://www.reddit.com/r/wallpapers.rss | grep -Eo "https://?[^&>]+(jpg|png)" | grep -Ev  "(thumbs|icon)" | head -1);
 NAME=$(basename "$URL");
-
-if [ "$DIR" ]
-  then
-    FILENAME="$DIR""$NAME";
-  else
-    FILENAME=~/Pictures/"$NAME";
-fi
-
-wget "$URL" -O "$FILENAME";
+FILENAME=~/Pictures/Wallpapers/"$NAME";
+wget "$URL" -qO "$FILENAME";
 sleep 1;
-gsettings set org.gnome.desktop.background picture-uri nothing;
-gsettings set org.gnome.desktop.background picture-uri file://"$FILENAME";
+gsettings set org.mate.background picture-filename "$FILENAME"
