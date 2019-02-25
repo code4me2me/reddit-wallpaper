76be177fb13bc54b0f7401bab64fe181380bba47
code4me2me <32439114+code4me2me@users.noreply.github.com>
2019-02-24 21:33:41 -0500

    quiet wget, check if file already exists

--- wallpaper.sh
+++ wallpaper.sh
@@ -2,6 +2,10 @@
 URL=$(wget -qO - http://www.reddit.com/r/wallpapers.rss | grep -Eo "https://?[^&>]+(jpg|png)" | grep -Ev  "(thumbs|icon)" | head -1);
 NAME=$(basename "$URL");
 FILENAME=~/Pictures/Wallpapers/"$NAME";
-wget "$URL" -qO "$FILENAME";
-sleep 1;
-gsettings set org.mate.background picture-filename "$FILENAME"
+if [ -f $FILENAME ]; then
+   echo "The file alredy exists."; exit
+    else
+        wget "$URL" -qO "$FILENAME";
+        sleep 1;
+        gsettings set org.mate.background picture-filename "$FILENAME"
+fi
